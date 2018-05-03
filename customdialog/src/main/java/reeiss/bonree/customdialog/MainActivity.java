662a3a3d0bbccf0b07a1c8092e99f7a7506b0c07
GodManRui <854463112@qq.com>
2018-05-03 18:10:26 +0800

    2222

--- customdialog/src/main/java/reeiss/bonree/customdialog/MainActivity.java
+++ customdialog/src/main/java/reeiss/bonree/customdialog/MainActivity.java
@@ -5,23 +5,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.RadioButton;
-import reeiss.bonree.customdialog.Rotate3dAnimation;
-public class MainActivity extends AppCompatActivity {
+import android.widget.RelativeLayout;
 
-    private AutoDisplayChildViewContainers auto;
+import reeiss.bonree.customdialog.Rotate3dAnimation;
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+  //  private AutoDisplayChildViewContainers auto;
+    private RelativeLayout layout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        auto = findViewById(R.id.vg);
+        setContentView(R.layout.first);
+
+        layout1 = (RelativeLayout) findViewById(R.id.layout1);
+        ImageView first_btn = (ImageView) findViewById(R.id.first_btn);
+        first_btn.setOnClickListener(this);
+       /* auto = findViewById(R.id.vg);
         for (int i = 0; i < 1; i++) {
             RadioButton radioButton = (RadioButton) LayoutInflater.from(this).inflate(R.layout.net_task_infodata_radiobutton, null);
             radioButton.setTag(i);
             radioButton.setText("Button " + (i + 1));
             auto.addView(radioButton);
-        }
+        }*/
 
     }
 
@@ -41,12 +50,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void rotateOnXCoordinate(int fromDegrees, int toDegrees, Byte rotateXAxis) {
-        float centerX = auto.getWidth() / 2.0f;
-        float centerY = auto.getHeight() / 2.0f;
+        float centerX = layout1.getWidth() / 2.0f;
+        float centerY = layout1.getHeight() / 2.0f;
         float depthZ = 0f;
         Rotate3dAnimation rotate3dAnimationX = new Rotate3dAnimation(fromDegrees, toDegrees, centerX, centerY, depthZ, rotateXAxis, true);
         rotate3dAnimationX.setDuration(1000);
-        auto.startAnimation(rotate3dAnimationX);
+        layout1.startAnimation(rotate3dAnimationX);
     }
 
     // 以Z轴为轴心旋转---等价于普通平面旋转动画
@@ -57,5 +66,15 @@ public class MainActivity extends AppCompatActivity {
          RotateAnimation rotateAnimation = new RotateAnimation(0, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
          rotateAnimation.setDuration(1000);
          auto.startAnimation(rotateAnimation);*/
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        start(v);
     }
 }
