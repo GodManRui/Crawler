b24507ba80b8a054ef6f5a149ce30682c3fbf697
GodManRui <854463112@qq.com>
2018-09-05 18:57:48 +0800

    第四个

--- ble_test/src/main/java/reeiss/bonree/ble_test/smarthardware/MainActivity.java
+++ ble_test/src/main/java/reeiss/bonree/ble_test/smarthardware/MainActivity.java
@@ -1,9 +1,12 @@
 package reeiss.bonree.ble_test.smarthardware;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
 import reeiss.bonree.ble_test.R;
@@ -23,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainbaidu);
+        Log.e("JerryZhuMM", "Main onCreate: " + savedInstanceState);
         initView();
+        if (savedInstanceState == null) {
+            mBottomNavigationView.setSelectedItemId(R.id.tab_menu_home);
+        }
     }
 
     private void initView() {
@@ -32,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.e("JerryZhuMM", "Main setOnNavigationItemSelectedListener: " + item.getItemId());
                 onTabItemSelected(item.getItemId());
                 return true;
             }
         });
-        onTabItemSelected(R.id.tab_menu_home);
-
+//        onTabItemSelected(R.id.tab_menu_home);
     }
 
     private void onTabItemSelected(int id) {
@@ -58,4 +65,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("JerryZhuMM", " Main onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("JerryZhuMM", " Main onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("JerryZhuMM", " Main onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.e("JerryZhuMM", " Main onSaveInstanceState(Bundle outState");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("JerryZhuMM", " Main onPause");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.e("JerryZhuMM", " Main onNewIntent");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("JerryZhuMM", " Main onResume");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.e("JerryZhuMM", " Main onRestoreInstanceState(Bundle savedInstanceState)");
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        Log.e("JerryZhuMM", " Main onRestoreInstanceState(Bundle savedInstanceState  PersistableBundle persistentState)");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("JerryZhuMM", " Main onRestart");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.e("JerryZhuMM", " Main onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) ");
+    }
 }
